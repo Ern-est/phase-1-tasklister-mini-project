a2abd179ce476a183ef5c631d8aa5dcaee220d21
Ernest <ernest.cheruiyot@student.moringaschool.com>
2022-12-13 11:02:07 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleTask(e.target.createTask.value)
+    form.reset()
+  })
 });
+function handleTask(task){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.textContent = "x";
+  btn.addEventListener('click', handleDelete)
+  li.textContent = `${task} `;
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
